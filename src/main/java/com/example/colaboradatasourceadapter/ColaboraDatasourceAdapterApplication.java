34949671d91e3965cbb47582e2e97be3ea8b4ade
Sebastian Droguett <sdroguett10@gmail.com>
2024-02-09 22:05:36 -0300

    Project creation

---
+++ src/main/java/com/example/colaboradatasourceadapter/ColaboraDatasourceAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.example.colaboradatasourceadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboraDatasourceAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColaboraDatasourceAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/colaboradatasourceadapter/ColaboraDatasourceAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colaboradatasourceadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboraDatasourceAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
