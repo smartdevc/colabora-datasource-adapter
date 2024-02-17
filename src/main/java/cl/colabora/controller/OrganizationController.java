34b668d5f2ecb5f60a216720d0d265d7d833ed21
daniel <daniel.soto48@inacapmail.cl>
2024-02-17 19:05:45 -0300

    information is still lacking

--- src/main/java/cl/colabora/controller/OrganizationController.java
+++ src/main/java/cl/colabora/controller/OrganizationController.java
@@ -1,4 +1,26 @@
 package cl.colabora.controller;
 
+import cl.colabora.model.Organization;
+import cl.colabora.service.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orgganization")
 public class OrganizationController {
+
+    @Autowired
+    private OrganizationService organizationService;
+
+    @GetMapping("/organizations")
+    public ResponseEntity <List<Organization>> getAllOrganizations(){
+        List<Organization> organizations = organizationService.getAllOrganizations();
+        return new ResponseEntity<>(organizations, HttpStatus.OK);
+    }
 }
--- src/main/java/cl/colabora/controller/UserController.java
+++ src/main/java/cl/colabora/controller/UserController.java
@@ -1,7 +1,31 @@
 package cl.colabora.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import cl.colabora.model.User;
+import cl.colabora.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUser(){
+        List<User> users = userService.getAllUser();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+
+
+
+
+    @DeleteMapping ("{id}")
+    public User deleteById(@PathVariable("id") Long id){
+        boolean ok = this.userService.deleteUser(id);
+        }
+    }
 }
