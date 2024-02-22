00102e771a6164bc19488262b7db6742d2e2c908
daniel <daniel.soto48@inacapmail.cl>
2024-02-21 22:06:59 -0300

    logic done. update method performed on all controllers.

--- src/main/java/cl/colabora/service/impl/UserServiceImpl.java
+++ src/main/java/cl/colabora/service/impl/UserServiceImpl.java
@@ -4,11 +4,14 @@ package cl.colabora.service.impl;
 import cl.colabora.model.User;
 import cl.colabora.repository.UserRepository;
 import cl.colabora.service.UserService;
+import jakarta.persistence.NoResultException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -29,16 +32,34 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User updateUser(User user) {
-        return null;
+        Optional<User> userFinded = userRepository.findById(user.getId());
+        if (userFinded.isPresent()){
+            User userUpdated = userFinded.get();
+            userUpdated.setName(user.getName());
+            userRepository.save(userUpdated);
+            return userUpdated;
+        } else {
+            throw new NoResultException("User with id" + user.getId() + "was not found");
+        }
     }
 
     @Override
     public User getUserById(Long id) {
-        return null;
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new NoResultException("User with id" + id + "was not found");
+        }
     }
 
     @Override
     public void deleteUser(Long id) {
-
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()){
+            userRepository.delete(user.get());
+        } else {
+            throw new NoResultException("User with id" + id + "was not found");
+        }
     }
 }
